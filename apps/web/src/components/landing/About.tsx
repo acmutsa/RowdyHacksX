93a843ba65e6458efc883e54aae3157983bb5db6
carter_lavigne <carterlavigne3@gmail.com>
2024-09-05 16:02:46 -0500

    test

--- apps/web/src/components/landing/About.tsx
+++ apps/web/src/components/landing/About.tsx
@@ -4,7 +4,7 @@ import D1 from "../../../public/img/landing/d1.svg";
 import D2 from "../../../public/img/landing/d2.svg"
 import D3 from "../../../public/img/landing/d3.svg";
 import D4 from "../../../public/img/landing/d4.svg";
-import Dino_Coding from "../../../public/img/landing/dinos_coding.png"
+import rh_logo from "../../../public/img/logo/rhbttf.png"
 export default function About() {
   const d1_stylesheet = {
     width:'25rem',
@@ -15,14 +15,75 @@ export default function About() {
     <section
       className="flex w-full items-center justify-center min-h-screen border-y-2 border-muted-foreground"
       id="About">
-      <div className="w-full flex flex-col items-center justify-center">
-        <h1 className=" text-4xl md:text-5xl font-black text-center">
-          About Section
-        </h1>
-        <h3 className="text-center font-bold text-lg md:text-2xl px-4 md:px-0">
-          Introduce the hackathon and its purpose! Make it sound enticing
-        </h3>
-      </div>
+      <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2">
+          <div className="flex flex-col justify-center gap-y-6 bg-white border-orange-500 border-4 rounded-xl p-5">
+            <h1 className="font-oswald text-center text-5xl font-bold italic text-orange-500 md:text-left lg:text-6xl">
+              ABOUT US
+            </h1>
+            <p className="text-center font-mono text-lg font-bold text-orange-500 md:text-left">
+              <Balancer>
+                RowdyHacks is a free, weekend-long, overnight hackathon hosted
+                at UTSA! Students can join us to network, code, collaborate, and
+                compete. We welcome hackers from all disciplines, backgrounds, &
+                technical levels!
+              </Balancer>
+            </p>
+          </div>
+          {/* Placeholder untill image is decided */}
+          <Image
+            src={rh_logo}
+            alt="rowdy hacks logo"
+            className="w-80 sm:w-96 md:w-[25rem] lg:w-[26rem] 2xl:w-[34rem] mx-auto"
+          />          
+          {/* Placeholder untill image is decided */}
+          <Image
+            src={rh_logo}
+            alt="rowdy hacks logo"
+            className="w-80 sm:w-96 md:w-[25rem] lg:w-[26rem] 2xl:w-[34rem] mx-auto"
+          />
+          <div className="flex flex-col justify-center gap-y-10 bg-white border-orange-500 border-4 rounded-xl p-5">
+            <h1 className="font-black text-6xl text-orange-500 text-center">
+              Celebrating 10 Years
+              <br />
+              of RowdyHacks
+            </h1>
+            <p className="text-center font-mono text-lg font-bold text-orange-500 md:text-left">
+              <Balancer>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula
+                risus non dui blandit, non ornare felis interdum. Integer fermentum eros vitae
+                leo fermentum, ac fermentum erat sollicitudin. Curabitur accumsan molestie
+                turpis, nec elementum lectus dictum vel. Nunc quis orci vel arcu iaculis
+                vehicula. Sed finibus sagittis ex, quis cursus sem ornare at. Maecenas eget eros
+                tellus. Vestibulum id libero sit amet magna rhoncus fermentum nec et velit.
+                Fusce varius nisl sit amet consequat fermentum.
+              </Balancer>
+            </p>
+            <h2 className="text-xl text-orange-500 font-mono font-bold">🧡 The RowdyHacks Team</h2>
+          </div>
+
+          <div className="pb-20 md:pb-0 flex flex-col justify-center">
+            <div className="flex flex-col justify-center gap-y-10 bg-white border-orange-500 border-4 rounded-xl p-5">
+              <h1 className="font-oswald text-center text-5xl font-bold italic text-orange-500 md:text-left lg:text-6xl">
+                WHO CAN ATTEND?
+              </h1>
+              <p className="text-center font-mono text-lg font-bold text-orange-500 md:text-left">
+                <Balancer>
+                  We're excited to welcome hackers from all disciplines,
+                  backgrounds, & technical levels! Whether you can't count the
+                  number of apps you've built, or you have never written a line
+                  of code before, RowdyHacks has something for everyone!
+                </Balancer>
+              </p>
+            </div>
+          </div>
+          {/* Placeholder untill image is decided */}
+          <Image
+            src={rh_logo}
+            alt="rowdy hacks logo"
+            className="w-80 sm:w-96 md:w-[25rem] lg:w-[26rem] 2xl:w-[34rem] mx-auto"
+          />
+        </div>
     </section>
+    
   );
 }
