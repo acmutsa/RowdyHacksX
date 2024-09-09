b9d4597fa55b500acedacd42003f479787ff9faa
Carter LaVigne <carterlavigne3@gmail.com>
2024-09-09 16:41:47 -0500

    test

--- apps/web/src/components/landing/About.tsx
+++ apps/web/src/components/landing/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
     >
       <div className="container mx-auto grid grid-cols-1 gap-y-16 gap-x-10 md:grid-cols-2">
         {/* ABOUT US Section */}
-        <div className="flex flex-col justify-center gap-y-6 bg-white border-[#ea580c] border-4 rounded-xl p-5 transform transition-transform duration-300 hover:scale-105">
+        <div className="flex flex-col justify-center gap-y-6 bg-white border-[#ea580c] border-4 rounded-xl p-8 transform transition-transform duration-300 hover:scale-105">
           <h1 className="font-oswald text-center text-3xl font-bold italic text-[#ea580c] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
             ABOUT US
           </h1>
@@ -29,7 +29,7 @@ export default function About() {
         </div>
 
         {/* WHO CAN ATTEND Section */}
-        <div className="flex flex-col justify-center gap-y-6 bg-white border-[#ea580c] border-4 rounded-xl p-5 transform transition-transform duration-300 hover:scale-105">
+        <div className="flex flex-col justify-center gap-y-6 bg-white border-[#ea580c] border-4 rounded-xl p-8 transform transition-transform duration-300 hover:scale-105">
           <h1 className="font-oswald text-center text-3xl font-bold italic text-[#ea580c] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
             WHO CAN ATTEND?
           </h1>
@@ -45,7 +45,7 @@ export default function About() {
 
       {/* Celebrating 10 Years Section */}
       <div className="container mx-auto mt-16 grid grid-cols-1 gap-y-16 md:grid-cols-1">
-        <div className="flex flex-col justify-center gap-y-10 bg-white border-[#ea580c] border-4 rounded-xl p-5 transform transition-transform duration-300 hover:scale-105">
+        <div className="flex flex-col justify-center gap-y-10 bg-white border-[#ea580c] border-4 rounded-xl p-8 transform transition-transform duration-300 hover:scale-105">
           <h1 className="font-oswald text-center text-3xl font-bold italic text-[#ea580c] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
             CELEBRATING 10 YEARS 
             <br /> 
