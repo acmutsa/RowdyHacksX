17ad8ecee6c98763e9740abdb1e1f2d6a87f8bee
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-10-24 14:39:46 -0500

    Added sponsors section to the website

--- apps/web/src/lib/utils/shared/types.ts
+++ apps/web/src/lib/utils/shared/types.ts
@@ -1,1 +1,10 @@
 export type DeArray<T> = T extends (infer R)[] ? R : T;
+
+type PartnerType = "gold" | "silver" | "bronze" | "title" | "partner";
+
+export interface Partner {
+	name: string;
+	logo: string;
+	tier: PartnerType;
+	url: string;
+}
