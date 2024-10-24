07e70683e8cff91fc88b50d5d072e9f1664c16d4
Joshua Silva <72359611+joshuasilva414@users.noreply.github.com>
2024-10-24 16:48:33 -0500

    Update survival guide link

--- packages/config/hackkit.config.ts
+++ packages/config/hackkit.config.ts
@@ -858,7 +858,7 @@ const c = {
 		facebook: "https://facebook.com/rowdyhacks",
 		twitter: "https://twitter.com/rowdyhacks",
 		github: "https://github.com/acmutsa",
-		guide: "https://go.rowdyhacks.org/discord",
+		guide: "https://guide.rowdyhacks.org",
 	},
 	icon: {
 		sm: "/img/logo/rhbttf.svg",
